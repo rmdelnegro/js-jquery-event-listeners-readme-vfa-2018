27ecba0731b8a89254828a3f6cfb70edebc0b7c2
Ralph Del Negro <ralphmdelnegro@gmail.com>
2018-06-28 21:07:36 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -10,9 +10,10 @@ function getIt(){
 function framIt(){
   $('img .tasty').on('load',
   function(){
-    
-  }
+    $(this).css('border', "solid 2px red");
+  });
 }
+
 $(document).ready(function(){
 
 // call functions here
